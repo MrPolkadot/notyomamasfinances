71a84c3c7d543df935e2797075e76095729e2d28
Jacob Maughan <135153003+jacslimob@users.noreply.github.com>
2023-09-23 16:46:16 -0600

    added user, expenses, category and bill models

---
+++ models/Expenses.js
@@ -0,0 +1,31 @@
+// Might be the same as the 'category model'
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Expenses extends Model {}
+
+Expenses.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    expense_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'expenses',
+  }
+);
+
+module.exports = Expenses;
