191f2642084e5a02120aefb13bd1a9c4ff5a63a6
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-09-24 22:43:10 -0600

    installed dotenv package

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST || process.env.host,
       dialect: 'mysql',
       port: 3306
     }
