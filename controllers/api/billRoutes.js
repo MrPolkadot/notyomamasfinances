e95e1f73e622875aa87a085d03ce6da4f052be68
Jacob Maughan <135153003+jacslimob@users.noreply.github.com>
2023-09-26 20:25:29 -0600

    added routes in all route files

--- controllers/api/billRoutes.js
+++ controllers/api/billRoutes.js
@@ -1,8 +1,39 @@
 const router = require("express").Router();
-
 const { Bills } = require("../../models");
+const withAuth = require("../utils/auth");
 
-
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newBills = await Bills.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newBills);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const billsData = await Savings.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!billsData) {
+        res.status(404).json({ message: 'No project found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(billsData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
 module.exports = router;
