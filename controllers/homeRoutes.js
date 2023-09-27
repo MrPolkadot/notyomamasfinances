727474a90a3c444fd942a6732d0d3d152ac92537
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-09-26 22:09:29 -0600

    Added more views

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,14 +4,8 @@ const { Bills, Expenses, Savings, User } = require("../models")
 
 const withAuth = require("../utils/auth");
 
-router.get("/", async (req, res) => {
-    try {
-        await res.render('login');
-        console.log("rendered")
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+
+
 
 
 
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const routes = require('./controllers'); //Create routes
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ defaultLayout: 'main' }); //This will use main.handlebars as our main layout page.
+const hbs = exphbs.create({ defaultLayout: "main" }); //This will use main.handlebars as our main layout page.
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -32,15 +32,13 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 //app.set('views', path.join(__dirname, './views/layouts'))
 
+app.use(routes);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'dist')))
 
-app.use(routes);
 
-app.get('/', (req, res) => {
-  
-});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
