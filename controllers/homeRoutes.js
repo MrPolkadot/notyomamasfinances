d95564bea434b4e1a75c0ce6b9d683a49797b835
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-09-26 22:18:16 -0600

    had merge issues

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,37 +2,43 @@ const router = require("express").Router();
 const { Bills, Expenses, Savings, User } = require("../models")
 const withAuth = require("../utils/auth");
 
-
-
+router.get("/", async (req, res) => {
+    try {
+        await res.render('login');
+        console.log("rendered")
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
     try {
-      // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Project }],
-      });
-  
-      const user = userData.get({ plain: true });
-  
-      res.render('profile', {
-        ...user,
-        logged_in: true
-      });
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Project }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
     } catch (err) {
-      res.status(500).json(err);
+        res.status(500).json(err);
     }
-  });
-  
-  router.get('/login', (req, res) => {
+});
+
+router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/profile');
-      return;
+        res.redirect('/profile');
+        return;
     }
-  
+
     res.render('login');
-  });
+});
 
 module.exports = router;
