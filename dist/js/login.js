6757abc32aa55e0a3e3f8c277a04580384eec8e9
Jacob Maughan <135153003+jacslimob@users.noreply.github.com>
2023-09-27 19:40:06 -0600

    added js folder to dist with signup, loginin, logout files

---
+++ dist/js/login.js
@@ -0,0 +1,33 @@
+// Needs to be structured for this project
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to whereever you want
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+
