19ad372291df7dfa3a02b65e3c20291b05bc943d
Jacob Maughan <135153003+jacslimob@users.noreply.github.com>
2023-09-27 21:02:13 -0600

    modified homeRoutes and added cookies and scripts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,15 +26,15 @@ router.get('/profile', withAuth, async (req, res) => {
         // Find the logged in user based on the session ID
         const userData = await User.findByPk(req.session.user_id, {
             attributes: { exclude: ['password'] },
-            include: [{ model: Project }],
+            
         });
 
         const user = userData.get({ plain: true });
 
         res.render('profile', {
             ...user,
-            logged_in: true
-        });
+            logged_in: req.session.logged_in});
+        
     } catch (err) {
         res.status(500).json(err);
     }
