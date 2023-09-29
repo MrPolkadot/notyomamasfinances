db7aef38f36fc2b779a11c90167b015a8c28bc49
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-09-28 19:42:58 -0600

    updated route names

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 
 const billRoutes = require("./billRoutes");
 const expensesRoutes = require("./expensesRoutes");
-const savingsRoutes = require("./savingsRoutes");
+const savingsRoutes = require("./incomeRoutes");
 const userRoutes = require("./userRoutes");
 
 router.use("/bills", billRoutes);
 router.use("/expenses", expensesRoutes);
-router.use("/savings", savingsRoutes);
+router.use("/income", savingsRoutes);
 router.use("/users", userRoutes);
 
 module.exports = router;
