0d152c1b2c6d989c69b5e152fcd5d65226dc0705
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-09-30 15:07:05 -0600

    Added dollar signs

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,10 @@
+var numeral = require("numeral");
+
 module.exports = {
     format_date: (date) => {
         return date.toLocaleDateString();
+    },
+    format_amount: (amount) => {
+        return numeral(amount).format("$0,0.00")
     }
 }
