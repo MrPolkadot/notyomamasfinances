ee9225c2c368bfc0d69c782b82927260a47732dc
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-09-30 13:50:53 -0600

    added input forms

--- dist/js/expense.js
+++ dist/js/expense.js
@@ -1,23 +1,23 @@
 const addExpense = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const expense_name = document.querySelector('#name').value.trim();
-    const expense_date = document.querySelector('#date').value.trim();
-    const amount = document.querySelector('#amount').value.trim();
+  const expense_name = document.querySelector('#name').value.trim();
+  const expense_date = document.querySelector('#date').value.trim();
+  const amount = document.querySelector('#amount').value.trim();
 
-    if(expense_name && expense_date && amount) {
-        const response = await fetch('/api/expenses', {
-            method: 'POST',
-            body: JSON.stringify({ expense_name, expense_date, amount }),
-            headers: { 'Content-Type': 'application/json' },
-          });
-      
-          if (response.ok) {
-            // If successful, redirect the browser to whereever you want
-            document.location.replace('/expense');
-          } else {
-            alert(response.statusText);
-          }
+  if (expense_name && expense_date && amount) {
+    const response = await fetch('/api/expenses', {
+      method: 'POST',
+      body: JSON.stringify({ expense_name, expense_date, amount }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to whereever you want
+      document.location.replace('/expenses');
+    } else {
+      alert(response.statusText);
     }
+  }
 }
 document.querySelector('#form').addEventListener('submit', addExpense);
