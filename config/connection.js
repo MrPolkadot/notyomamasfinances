580edf9362607e5440783eaa1888bbf0d3839970
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-10-01 13:14:49 -0600

    test

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: "localhost",
     dialect: 'mysql',
     port: 3306
   }
