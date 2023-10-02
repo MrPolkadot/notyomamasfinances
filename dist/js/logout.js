b3bc84d34724c9229636e01dbf907d4d7ded93cf
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-10-01 22:05:59 -0600

    fixed logout for small screens. updated readme.

--- dist/js/logout.js
+++ dist/js/logout.js
@@ -11,4 +11,8 @@ const logout = async () => {
   }
 };
 
+
+
 document.querySelector('#logout').addEventListener('click', logout);
+
+document.querySelector('#small-logout').addEventListener('click', logout);
